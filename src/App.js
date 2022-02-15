fc681388ce38f085eb01e3a9470d3bdf1fdac6a7
creativenothing <creative@nothing>
2022-02-15 15:56:04 -0500

    add Navbar, TweetList, Button, fetch request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Triangle } from "react-loader-spinner"
+import Navbar from "./components/Navbar"
+
+const API_PATH = "http://192.168.1.190:8000/api/"
+
+const TweetList = props => {
+	const { tweetlist } = props
+	return (
+		<ul>
+			{tweetlist.map((t, i) => (
+				<li key={i}>{t}</li>
+			))}
+		</ul>
+	)
 }
 
-export default App;
+const Loading = () => {
+	return (
+		<div className="loading">
+			<Triangle color="#FE5000" height="100" width="100" />
+		</div>
+	)
+}
+
+const App = () => {
+	const [tweetlist, setTweetlist] = useState([])
+	const [isLoading, setLoading] = useState(false)
+
+	const fetchTweets = () => {
+		setLoading(true)
+		axios.get(API_PATH).then(({ data }) => {
+			setLoading(false)
+			setTweetlist(data)
+		})
+	}
+	return (
+		<div className="tiqqun-ai container">
+			<Navbar />
+			{isLoading ? <Loading /> : <TweetList tweetlist={tweetlist} />}
+
+			<button onClick={fetchTweets}>Fetch Tweets</button>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = props => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<strong>Tiqqun Twitter AI</strong>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<a href="https://twitter.com/chaoticautomata">twitter</a>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
