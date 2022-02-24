abffbda00fad9eba6daaf813456ea1bd6258fa40
creativenothing <creative@nothing>
2022-02-24 02:33:03 -0500

    update fetchNews function to fetch covid news

--- src/utils/fetchNews.js
+++ src/utils/fetchNews.js
@@ -14,27 +14,38 @@ const axios = require("axios")
 const fs = require("fs")
 
 const NEWS_API_PATH = "https://newsapi.org/v2/top-headlines"
-const NEWS_API_KEY = process.env.NEWSAPI_API_KEY
+const NEWS_API_KEY = process.env.REACT_APP_NEWSAPI_KEY
+console.log("THIS SHOULD BE THE KEY", NEWS_API_KEY)
+//todo change to general
+//const filepath = __dirname + "/../data/news/" + slug + ".json"
 
+const sources =
+	"abc-news,al-jazeera-english,associated-press,axios,bbc-news,breitbart,cbc-news,cnn,fox-news,google-news,msnbc,national-review,nbc-news,newsweek,politico,reuters,the-american-conservative,the-hill,the-washington-post,usa-today,vice-news"
+
+const headers = { "x-api-key": NEWS_API_KEY }
 const params = {
 	apiKey: NEWS_API_KEY,
-	country: "us",
 	language: "en",
+	sources,
 	pageSize: 100
 }
 
-const fetchNews = params =>
+console.log({ headers, params })
+const fetchCovidHeadlines = otherParams =>
 	axios
-		.get(NEWS_API_PATH, { params: params })
+		.get(NEWS_API_PATH, { headers, params: { q: "covid", ...otherParams } })
 		.then(({ data }) => data)
 		.catch(err => console.log(err))
 
+const fetchGenHeadlines = params =>
+	axios
+		.get(NEWS_API_PATH, { params })
+		.then(({ data }) => data)
+		.catch(err => console.log(err.message))
+
 const saveArticle = article => {
-	console.log(article.title)
-	console.log(slugify)
 	const slug = slugify(article.title)
-	const filepath = __dirname + "/news/" + slug + ".json"
-	console.log("FILEPATH: " + filepath)
+	const filepath = __dirname + "/../data/covid/" + slug + ".json"
 	fs.writeFileSync(filepath, JSON.stringify(article))
 }
 
@@ -47,12 +58,12 @@ const paginate = data => {
 	const pages = Math.ceil(totalResults / 10)
 	for (let page = 1; page < 2; page++) {
 		const newParams = { page, ...params }
-		fetchNews(newParams).then(data => saveResults(data))
+		fetchCovidHeadlines(newParams).then(data => saveResults(data))
 	}
 }
 
 const downloadAllNews = () => {
-	fetchNews(params)
+	fetchCovidHeadlines(params)
 		.then(data => {
 			saveArticle(data)
 			paginate(data)
