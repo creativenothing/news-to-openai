37b648329cf1b698ebb307702d54696966de5d42
creativenothing <creative@nothing>
2022-03-29 15:07:38 -0400

    add production routes to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const logger = require('morgan')
 const port = process.env.PORT || 5000
 
+const PUBLIC_PATH = path.join(__dirname, '..', 'build')
 const twitter = require('./routes/twitter')
 
 const app = express()
@@ -25,6 +26,11 @@ app.use(
 app.use(cors())
 
 app.use('/twitter', twitter)
+
+app.use(express.static(PUBLIC_PATH))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(PUBLIC_PATH, 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`l337 haxing happening on port ${port}`)
