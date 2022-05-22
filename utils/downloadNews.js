cb91bed6fbe9530b38446c65a928d8edc6e2967b
creativenothing <creative@nothing>
2022-05-22 10:35:02 -0400

    prettify utils/downloadNews.js

--- utils/downloadNews.js
+++ utils/downloadNews.js
@@ -8,87 +8,87 @@
  *
  *  the key can be found in .env
  */
-const fs = require("fs")
-const path = require("path")
-const axios = require("axios")
-const slugify = require("react-slugify").default
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const slugify = require('react-slugify').default
 
-const NEWS_API_PATH = "https://newsapi.org/v2/top-headlines"
+const NEWS_API_PATH = 'https://newsapi.org/v2/top-headlines'
 const NEWS_API_KEY = process.env.REACT_APP_NEWSAPI_KEY
 
 const sources =
-	"abc-news,al-jazeera-english,associated-press,axios,bbc-news,breitbart,cbc-news,cnn,fox-news,google-news,msnbc,national-review,nbc-news,newsweek,politico,reuters,the-american-conservative,the-hill,the-washington-post,usa-today,vice-news"
+  'abc-news,al-jazeera-english,associated-press,axios,bbc-news,breitbart,cbc-news,cnn,fox-news,google-news,msnbc,national-review,nbc-news,newsweek,politico,reuters,the-american-conservative,the-hill,the-washington-post,usa-today,vice-news'
 
-const headers = { "x-api-key": NEWS_API_KEY }
+const headers = { 'x-api-key': NEWS_API_KEY }
 
 const defaultParams = {
-	language: "en",
-	sources,
-	pageSize: 100
+  language: 'en',
+  sources,
+  pageSize: 100
 }
 
-const DATADIR = path.resolve(__dirname, "../data/news/")
-const NEWSFILEPATH = path.resolve(__dirname, "../public/news.json")
-let id = fs.readdirSync(__dirname + "/../data/news").length + 1
+const DATADIR = path.resolve(__dirname, '../data/news/')
+const NEWSFILEPATH = path.resolve(__dirname, '../public/news.json')
+let id = fs.readdirSync(__dirname + '/../data/news').length + 1
 
-console.log("data directory: " + DATADIR)
-console.log("newsfile path: " + NEWSFILEPATH)
-console.log("number of existing articles: " + id)
+console.log('data directory: ' + DATADIR)
+console.log('newsfile path: ' + NEWSFILEPATH)
+console.log('number of existing articles: ' + id)
 
 const fetchHeadlines = sentParams => {
-	const params = { ...defaultParams, ...sentParams }
-	console.log("Fetching headlines from " + NEWS_API_PATH)
-	return axios
-		.get(NEWS_API_PATH, { headers, params })
-		.then(({ data }) => {
-			console.log("Retrieved " + data.articles.length + " articles. ")
-			return data.articles
-		})
-		.catch(err => console.log(err))
+  const params = { ...defaultParams, ...sentParams }
+  console.log('Fetching headlines from ' + NEWS_API_PATH)
+  return axios
+    .get(NEWS_API_PATH, { headers, params })
+    .then(({ data }) => {
+      console.log('Retrieved ' + data.articles.length + ' articles. ')
+      return data.articles
+    })
+    .catch(err => console.log(err))
 }
 
 const saveArticle = article => {
-	article.id = id
-	id++
-	const slug = slugify(article.title)
-	const filepath = path.resolve(DATADIR, slug + ".json")
-	fs.writeFileSync(filepath, JSON.stringify(article))
+  article.id = id
+  id++
+  const slug = slugify(article.title)
+  const filepath = path.resolve(DATADIR, slug + '.json')
+  fs.writeFileSync(filepath, JSON.stringify(article))
 }
 
 const downloadAllHeadlines = () =>
-	fetchHeadlines()
-		.then(articles => articles.forEach(a => saveArticle(a)))
-		.catch(e => console.log(e))
+  fetchHeadlines()
+    .then(articles => articles.forEach(a => saveArticle(a)))
+    .catch(e => console.log(e))
 
 writeNewsFileFromDir = () => {
-	const filenames = fs.readdirSync(DATADIR)
-	const articles = filenames
-		.map(filename => fs.readFileSync(DATADIR + "/" + filename).toString())
-		.join(",")
-	console.log(
-		"Writing " + filenames.length + " articles to " + NEWSFILEPATH + "."
-	)
-	fs.writeFileSync(NEWSFILEPATH, "[")
-	fs.writeFileSync(NEWSFILEPATH, articles, { flag: "a" })
-	fs.writeFileSync(NEWSFILEPATH, "]", { flag: "a" })
+  const filenames = fs.readdirSync(DATADIR)
+  const articles = filenames
+    .map(filename => fs.readFileSync(DATADIR + '/' + filename).toString())
+    .join(',')
+  console.log(
+    'Writing ' + filenames.length + ' articles to ' + NEWSFILEPATH + '.'
+  )
+  fs.writeFileSync(NEWSFILEPATH, '[')
+  fs.writeFileSync(NEWSFILEPATH, articles, { flag: 'a' })
+  fs.writeFileSync(NEWSFILEPATH, ']', { flag: 'a' })
 }
 
 const fixIds = () => {
-	let iter = 1
-	const filenames = fs.readdirSync(DATADIR)
-	const articles = filenames
-		.map(filename => JSON.parse(fs.readFileSync(DATADIR + "/" + filename)))
-		.map(a => {
-			a.id = iter
-			iter++
-			return JSON.stringify(a)
-		})
-		.join(",")
+  let iter = 1
+  const filenames = fs.readdirSync(DATADIR)
+  const articles = filenames
+    .map(filename => JSON.parse(fs.readFileSync(DATADIR + '/' + filename)))
+    .map(a => {
+      a.id = iter
+      iter++
+      return JSON.stringify(a)
+    })
+    .join(',')
 
-	console.log(articles)
-	fs.writeFileSync(NEWSFILEPATH, "[")
-	fs.writeFileSync(NEWSFILEPATH, articles, { flag: "a" })
-	fs.writeFileSync(NEWSFILEPATH, "]", { flag: "a" })
+  console.log(articles)
+  fs.writeFileSync(NEWSFILEPATH, '[')
+  fs.writeFileSync(NEWSFILEPATH, articles, { flag: 'a' })
+  fs.writeFileSync(NEWSFILEPATH, ']', { flag: 'a' })
 }
 
 //fixIds()
